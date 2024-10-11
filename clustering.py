95c94c346fb2bd62409b49f8cb895eeaaa21ee67
the-ninth-wave <34815796+the-ninth-wave@users.noreply.github.com>
2024-10-11 14:13:58 -0400

    updated clustering and viz nb

--- clustering.py
+++ clustering.py
@@ -5,6 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+###
+# clustering
+###
+
 def extract_short_vectors(Q,R,T):
     """
     Input
@@ -123,10 +127,14 @@ def k_means_clustering(W, H, k):
 
     return labels_W, labels_H
 
+###
+# plotting
+###
 
-def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
-                      spacing=20, flipy=True, color_scheme='tab', \
-                      title=None, save_name=None, flip=False, cell_type_labels=None):
+def plot_cluster_pair(S1, S2, labels_W, labels_H, \
+                      cell_type_labels=None, dotsize=15, \
+                      spacing=20, color_scheme='tab', \
+                      title=None, save_name=None, flip=False):
     """
     Input
         S1 : np.ndarray, spatial coordinates for the first slice of shape (n, 2)
@@ -135,7 +143,6 @@ def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
         labels_H : np.ndarray, cluster labels for the second slice
         dotsize : int, size of the dots, default=15
         spacing : int, spacing around the dots, default=20
-        flipy : bool, whether to flip the y-axis, default=True
         color_scheme : str, color scheme for the clusters, default='tab', other options: 'rainbow'
         title : str, title for the plot, default=None
         save_name : str, file name to save the plot, default=None
@@ -146,6 +153,9 @@ def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
     Output
         Plots the two slices side by side with spots colored according to their labels from k-means clustering
     """
+    if cell_type_labels is None:
+        cell_type_labels = [None, None]
+    
     set_labels_W = set(labels_W)
     set_labels_H = set(labels_H)
     labels_union = set_labels_W.union(set_labels_H)
@@ -203,7 +213,7 @@ def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
 
         ax.set_xlim(xmin - spacing, xmax + spacing)
         ax.set_ylim(ymin - spacing, ymax + spacing)
-        if flipy:
+        if flip:
             ax.invert_yaxis()
         else:
             pass
@@ -211,14 +221,9 @@ def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
         ax.set_title(f'Slice {i+1}\n', color='black')
         ax.set_aspect('equal', adjustable='box')
 
-        if cell_type_labels[i] is not None and i == 0:
-            cell_type_labels_2 = cell_type_labels[i]
+        if cell_type_labels[i] is not None:
             handles, labels = ax.get_legend_handles_labels()
-            ax.legend(handles=handles, labels=cell_type_labels_2, title='')
-        elif cell_type_labels[i] is not None and i == 1:
-            cell_type_labels_3 = cell_type_labels[i]
-            handles, labels = ax.get_legend_handles_labels()
-            ax.legend(handles=handles, labels=cell_type_labels_3, title='')
+            ax.legend(handles=handles, labels=cell_type_labels[i], title='')
             
     plt.tight_layout()
     #plt.legend()
@@ -231,93 +236,11 @@ def plot_cluster_pair(S1, S2, labels_W, labels_H, dotsize=15, \
 
     plt.show()
 
-def plot_cluster_triple(S1, S2, S3, labels_W, labels_H, labels_I, color_scheme='tab', title=None, save_name=None, flip=False, dotsize=15):
-    """
-    Input
-        S1 : np.ndarray, spatial coordinates for the first slice of shape (n, 2)
-        S2 : np.ndarray, spatial coordinates for the second slice of shape (m, 2)
-        S3 : np.ndarray, spatial coordinates for the third slice of shape (o, 2)
-        labels_W : np.ndarray, cluster labels for the first slice
-        labels_H : np.ndarray, cluster labels for the second slice
-        labels_I : np.ndarray, cluster labels for the third slice
-        color_scheme : str, color scheme for the clusters, default='tab', other options: 'rainbow'
-        title : str, title for the plot, default=None
-        save_name : str, file name to save the plot, default=None
-        flip : bool, whether to flip the spatial coordinates, default=False
 
-    Output
-        Plots the three slices side by side with spots colored according to their labels from k-means clustering
-    """
-    set_labels_W = set(labels_W)
-    set_labels_H = set(labels_H)
-    set_labels_I = set(labels_I)
-    labels_union = set_labels_W.union(set_labels_H).union(set_labels_I)
-    labels_intersection = set_labels_W.intersection(set_labels_H).intersection(set_labels_I)
-    print(f'Number of clusters in slice 1: {len(set_labels_W)}')
-    print(f'Number of clusters in slice 2: {len(set_labels_H)}')
-    print(f'Number of clusters in slice 3: {len(set_labels_I)}')
-    print(f'Number of clusters in common: {len(labels_intersection)}')
-    print(f'Number of clusters total: {len(labels_union)}')
-    num_clusters = len(labels_union)
-    sns.set_style("darkgrid")
-    sns.set_context("notebook", font_scale=1.5)
-    fig, axes = plt.subplots(1, 3, figsize=(30, 20), facecolor='white')
-
-    slices = [S1, S2, S3]
-    labels = [labels_W, labels_H, labels_I]
-    unique_values = np.unique(np.concatenate(labels))
-
-    if color_scheme == 'tab':
-        cmap_tab20 = plt.get_cmap('tab20')
-        cmap_tab20b = plt.get_cmap('tab20b')
-
-        colors_tab20 = [cmap_tab20(i) for i in range(cmap_tab20.N)]
-        colors_tab20b = [cmap_tab20b(i) for i in range(cmap_tab20b.N)]
-
-        combined_colors = colors_tab20 + colors_tab20b
-        colors = [combined_colors[i % len(combined_colors)] for i in range(len(unique_values))]
-    else:
-        colors = [mcolors.hsv_to_rgb((i / num_clusters, 1, 1)) for i in range(num_clusters)]
-
-    # Make color_dict for either color scheme
-    color_dict = dict(zip(unique_values, colors))
-
-    # Determine the combined limits of the axes
-    all_spatial = np.vstack(slices)
-    x_min, x_max = np.min(all_spatial[:, 0]), np.max(all_spatial[:, 0])
-    y_min, y_max = np.min(all_spatial[:, 1]), np.max(all_spatial[:, 1])
-    
-    for i, (S, value_vec) in enumerate(zip(slices, labels)):
-        ax = axes[i]
-        ax.set_facecolor('black')
-
-        spatial = S if not flip else S @ np.array([[-1, 0], [0, 1]])
-        df = pd.DataFrame({'x': spatial[:, 0], 'y': spatial[:, 1], 'value': value_vec})
-
-        sns.scatterplot(
-            x='x', y='y', hue='value', palette=color_dict, data=df, ax=ax, s=dotsize, legend=False
-        )
-
-        ax.set_xlim(xmin - spacing, xmax + spacing)
-        ax.set_ylim(ymin - spacing, ymax + spacing)
-        if flipy:
-            ax.invert_yaxis()
-        else:
-            pass
-        ax.axis('off')
-        ax.set_title(f'Slice {i+1}\n', color='black')
-        ax.set_aspect('equal', adjustable='box')
-
-    plt.tight_layout()
-
-    if title:
-        plt.suptitle(title, fontsize=16, color='black')
-
-    if save_name:
-        plt.savefig(save_name, dpi=300, transparent=True, bbox_inches="tight", facecolor='black')
-    plt.show()
-
-def plot_cluster_list(spatial_list, cluster_list, color_scheme='tab', title=None, save_name=None, flip=False):
+def plot_cluster_list(spatial_list, 
+                      cluster_list, 
+                      cell_type_labels=None,
+                      color_scheme='tab', title=None, save_name=None, flip=False):
     """
     Input
         spatial_list : list of np.ndarray, spatial coordinates for the slices
@@ -331,6 +254,9 @@ def plot_cluster_list(spatial_list, cluster_list, color_scheme='tab', title=None
         Plots the three slices side by side with spots colored according to their labels from k-means clustering
     """
     N_slices = len(spatial_list)
+
+    if cell_type_labels is None:
+        cell_type_labels = [None]*N_slices
 
     sns.set_style("darkgrid")
     sns.set_context("notebook", font_scale=1.5)
@@ -371,7 +297,7 @@ def plot_cluster_list(spatial_list, cluster_list, color_scheme='tab', title=None
         df = pd.DataFrame({'x': spatial[:, 0], 'y': spatial[:, 1], 'value': value_vec})
 
         sns.scatterplot(
-            x='x', y='y', hue='value', palette=color_dict, data=df, ax=ax, s=100, legend=False
+            x='x', y='y', hue='value', palette=color_dict, data=df, ax=ax, s=100, legend=True
         )
 
         ax.set_xlim(x_min, x_max)
@@ -381,6 +307,10 @@ def plot_cluster_list(spatial_list, cluster_list, color_scheme='tab', title=None
         ax.axis('off')
         ax.set_title(f'Slice {i+1}\n', color='black')
         ax.set_aspect('equal', adjustable='box')
+
+        if cell_type_labels[i] is not None:
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(handles=handles, labels=cell_type_labels[i], title='')
 
     plt.tight_layout()
 
@@ -414,125 +344,6 @@ def get_color_scheme(cluster_list, color_scheme='tab'):
     color_dict = dict(zip(unique_values, colors))
     return color_dict
 
-'''
-def plot_multi_differentiation(population_list, 
-                               transition_list,
-                               label_list,
-                               color_dict, 
-                               dotsize_factor=400, 
-                               linethick_factor=10):
-    sns.set(style="white")  # Set the Seaborn style
-    dsf = dotsize_factor
-    ltf = linethick_factor
-
-    N_slices = len(population_list)
-
-    # form x_positions, y_positions, and colors for each slice
-    x_positions = []
-    y_positions = []
-    for i, population in enumerate(population_list):
-        y_positions.append(np.arange(len(population)))
-        x_positions.append(np.ones(len(population)) * (i))
-
-
-    plt.figure(figsize=(5 * (N_slices - 1), 20))  # Adjust the figure size
-
-    for pair_ind, T in enumerate(transition_list):
-        plt.scatter(x_positions[pair_ind], 
-                    y_positions[pair_ind],
-                    c=[color_dict[label] for label in label_list[pair_ind]],
-                    s=population_list[pair_ind],
-                    edgecolor='b',
-                    linewidth=1,
-                    zorder=1)
-        print(pair_ind + 1)
-        print(x_positions[pair_ind+1])
-        plt.scatter(x_positions[pair_ind+1], 
-                    y_positions[pair_ind+1],
-                    c=[color_dict[label] for label in label_list[pair_ind+1]],
-                    s=population_list[pair_ind+1],
-                    edgecolor='b',
-                    linewidth=1,
-                    zorder=1)
-        r1 = T.shape[0]
-        r2 = T.shape[1]
-        for i in range(r1):
-            for j in range(r2):
-                if T[i, j] > 0:  # Plot line only if T[i, j] is greater than 0
-                    plt.plot([x_positions[pair_ind][i], x_positions[pair_ind+1][j]], 
-                            [y_positions[pair_ind][i], y_positions[pair_ind+1][j]], 
-                            'k-', lw=T[i, j] * ltf, zorder=0)
-
-
-    # Add titles and labels
-    plt.title('Differentiation Map')
-    plt.yticks([])
-    plt.xticks([])
-    plt.axis('off')
-
-    # Remove the top and right spines
-    sns.despine()
-
-    plt.show()
-'''
-def plot_multi_differentiation(population_list, 
-                               transition_list,
-                               label_list,
-                               color_dict, 
-                               dotsize_factor=400, 
-                               linethick_factor=10):
-    sns.set(style="white")  # Set the Seaborn style
-    dsf = dotsize_factor
-    ltf = linethick_factor
-
-    N_slices = len(population_list)
-
-    # form x_positions, y_positions, and colors for each slice
-    x_positions = []
-    y_positions = []
-    for i, population in enumerate(population_list):
-        y_positions.append(np.arange(len(population)))
-        x_positions.append(np.ones(len(population)) * (i))
-
-
-    plt.figure(figsize=(5 * (N_slices - 1), 20))  # Adjust the figure size
-
-    for pair_ind, T in enumerate(transition_list):
-        plt.scatter(x_positions[pair_ind], 
-                    y_positions[pair_ind],
-                    c=[color_dict[label] for label in label_list[pair_ind]],
-                    s=dsf*population_list[pair_ind],
-                    edgecolor='b',
-                    linewidth=1,
-                    zorder=1)
-        
-        plt.scatter(x_positions[pair_ind+1], 
-                    y_positions[pair_ind+1],
-                    c=[color_dict[label] for label in label_list[pair_ind+1]],
-                    s=dsf*population_list[pair_ind+1],
-                    edgecolor='b',
-                    linewidth=1,
-                    zorder=1)
-        r1 = T.shape[0]
-        r2 = T.shape[1]
-        for i in range(r1):
-            for j in range(r2):
-                if T[i, j] > 0:  # Plot line only if T[i, j] is greater than 0
-                    plt.plot([x_positions[pair_ind][i], x_positions[pair_ind+1][j]], 
-                            [y_positions[pair_ind][i], y_positions[pair_ind+1][j]], 
-                            'k-', lw=T[i, j] * ltf, zorder=0)
-
-
-    # Add titles and labels
-    plt.title('Differentiation Map')
-    plt.yticks([])
-    plt.xticks([])
-    plt.axis('off')
-
-    # Remove the top and right spines
-    sns.despine()
-
-    plt.show()
 
 def plot_labeled_differentiation(population_list,
                                  transition_list,
@@ -585,9 +396,12 @@ def plot_labeled_differentiation(population_list,
     # Add node labels
     if node_labels is not None:
         for i in range(N_slices):
-            for j in range(len(population_list[i])):
-                plt.text(x_positions[i][j], y_positions[i][j], node_labels[i][j],
-                        fontsize=12, ha='right', va='bottom')
+            if node_labels[i] is not None:
+                for j in range(len(population_list[i])):
+                    plt.text(
+                        x_positions[i][j], y_positions[i][j], node_labels[i][j],
+                        fontsize=12, ha='right', va='bottom'
+                    )
 
     # Add titles and labels
     plt.title('Differentiation Map')
--- util_zf.py
+++ util_zf.py
@@ -1,8 +1,10 @@
 import numpy as np
 import torch
+from scipy.optimize import linprog
+
 
 ###
-# from zf nbs
+# from zf nbs 
 
 def factor_mats(C, A, B, device, z=None, c=100, nidx_1=None, nidx_2=None):    
     norm1 = c
@@ -33,3 +35,118 @@ def factor_mats(C, A, B, device, z=None, c=100, nidx_1=None, nidx_2=None):
                                        (V2_A.type(torch.DoubleTensor).to(device)/norm3, V2_B.type(torch.DoubleTensor).to(device)/norm3))
     
     return C_factors, A_factors, B_factors
+
+##
+# for "registering" cell types
+
+def confusion_matrix(partitionA, partitionB):
+    cm = np.zeros((len(partitionA), len(partitionB)))
+
+    for i in range(len(partitionA)):
+        for j in range(len(partitionB)):
+            cm[i,j] = len(partitionA[i] & partitionB[j])
+    return cm 
+
+def cm_custom(labels_A, labels_B):
+    labels_A = np.array(labels_A)
+    labels_B = np.array(labels_B)
+    
+    unique_A = np.unique(labels_A)
+    unique_B = np.unique(labels_B)
+    
+    cm = np.zeros((len(unique_A), len(unique_B)))
+
+    for i, itemA in enumerate(unique_A):
+        for j, itemB in enumerate(unique_B):
+            count = np.sum((labels_A == itemA) & (labels_B == itemB))
+            cm[i, j] = count
+            # print(f"Comparing {itemA} with {itemB}: count = {count}")
+    
+    return cm
+
+
+def get_assignment(C):
+    # Flatten the cost matrix for the objective function
+    n_samples=C.shape[0]
+    c = C.flatten()
+
+    a = np.ones(n_samples) / n_samples
+    b = np.ones(n_samples) / n_samples
+
+    # Equality constraints matrix A_eq and vector b_eq
+    # Constraints to ensure the sum of transported mass equals the probability vectors
+    A_eq = []
+    b_eq = []
+
+    # Supply constraints
+    for i in range(len(a)):
+        constraint = np.zeros(C.shape)
+        constraint[i, :] = 1
+        A_eq.append(constraint.flatten())
+        b_eq.append(a[i])
+
+    # Demand constraints
+    for j in range(len(b)):
+        constraint = np.zeros(C.shape)
+        constraint[:, j] = 1
+        A_eq.append(constraint.flatten())
+        b_eq.append(b[j])
+
+    A_eq = np.array(A_eq)
+    b_eq = np.array(b_eq)
+
+    # Bounds for each variable (all should be non-negative)
+    x_bounds = [(0, None) for _ in range(len(c))]
+
+    # Solve the linear programming problem
+    result = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=x_bounds, method='highs')
+
+    # Display the result
+    if result.success:
+        optimal_transport_plan = result.x.reshape(C.shape)
+        # print("Optimal Transport Plan:")
+        # print(optimal_transport_plan)
+        final_cost = np.sum(optimal_transport_plan * C)
+        """
+        print("Final Transport Cost:", final_cost)
+        plt.figure(figsize=(7, 6))
+        ax = sns.heatmap(optimal_transport_plan, annot=False, cmap='Blues')
+        plt.title("Optimal Transport Plan")
+        ax.xaxis.set_label_position('top')
+        ax.xaxis.tick_top()
+        plt.xlabel("Target Distribution b")
+        plt.ylabel("Source Distribution a")
+        plt.show()
+        """;
+    else:
+        print("Optimization failed:", result.message)
+        return None
+
+    return optimal_transport_plan
+
+def get_ct_registration_dict(assignment, pred_labels_num, anno_labels_num):
+    set_pl = set(pred_labels_num)
+    set_an = set(anno_labels_num)
+
+    ct_dict = {}
+
+    ind_to_anno = {j : label for j, label in enumerate(list(set_an))}
+
+    for pred_i, pred_label in enumerate(list(set_pl)):
+        ct_dict[pred_label] = ind_to_anno[np.argmax(assignment[pred_i,:])]
+    
+    return ct_dict
+
+def convert_to_gt_labels(pred_labels_num, anno_labels_num):
+
+    cm = cm_custom(pred_labels_num, anno_labels_num)
+
+    assignment = get_assignment(np.sum(cm) - cm)
+
+    ct_dict = get_ct_registration_dict(assignment,
+                                       pred_labels_num,
+                                       anno_labels_num)
+
+    labels_pred_converted = [ct_dict[label] for label in list(pred_labels_num)]
+
+    return np.array(labels_pred_converted)
