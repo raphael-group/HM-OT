223f9ad7bf6fbd9b0f65dc07898cc02720265946
the-ninth-wave <34815796+the-ninth-wave@users.noreply.github.com>
2024-10-15 08:33:51 -0400

    added code (util_zf) and nb for making Sankeys from Qs

--- util_zf.py
+++ util_zf.py
@@ -1,13 +1,21 @@
 import numpy as np
 import torch
 from scipy.optimize import linprog
+import pandas as pd
 
 import networkx as nx
 from sklearn.neighbors import kneighbors_graph
 from scipy.spatial import distance
 
-###
-# from zf nbs 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# in nb: !pip install -U plotly -q   # for making Sankey diagram
+# in nb: !pip install -U kaleido -q   # for saving Sankey diagram
+
+################################################################################################
+#   misc
+################################################################################################
 
 def factor_mats(C, A, B, device, z=None, c=100, nidx_1=None, nidx_2=None):    
     norm1 = c
@@ -38,6 +46,10 @@ def factor_mats(C, A, B, device, z=None, c=100, nidx_1=None, nidx_2=None):
                                        (V2_A.type(torch.DoubleTensor).to(device)/norm3, V2_B.type(torch.DoubleTensor).to(device)/norm3))
     
     return C_factors, A_factors, B_factors
+
+################################################################################################
+#   magic cost matrix
+################################################################################################
 
 def make_graph_from_coords(S, n_neighbors=4, draw_graph=False):
     G = kneighbors_graph(X=S,
@@ -146,3 +158,102 @@ def HDM_from_XS(S, X, n_neighbors=4, truncation=100, time=10.):
     eigenvectors, eigenvalues = make_Lap_ei(S, X, n_neighbors)
     HDM_stack = HDM_representation(S, eigenvectors, eigenvalues, truncation, time)
     return HDM_stack
+
+
+################################################################################################
+#   for Sankey diagrams
+################################################################################################
+
+def make_sankey(gt_clustering, pred_clustering, gt_labels, save_format='jpg', save_name=None, title=None):
+    df1 = pd.DataFrame({'GT clusters': gt_clustering, 'Predicted clusters': pred_clustering})
+    transition_matrix = pd.crosstab(df1['GT clusters'], df1['Predicted clusters'])
+
+
+    # Get unique cluster labels
+    gt_clusters = sorted(df1['GT clusters'].unique())
+    pred_clusters = sorted(df1['Predicted clusters'].unique())
+
+    # Define node labels
+    labels = gt_labels + [f'Predicted Cluster {i}' for i in pred_clusters]
+
+
+    # Number of clusters
+    num_gt_clusters = len(gt_clusters)
+    num_pred_clusters = len(pred_clusters)
+
+    # Function to generate colors
+    def generate_colors(num_colors, colormap_name):
+        cmap = plt.get_cmap(colormap_name)
+        colors = cmap(np.linspace(0, 1, num_colors))
+        return ['#{:02x}{:02x}{:02x}'.format(int(r*255), int(g*255), int(b*255)) for r, g, b, _ in colors]
+
+    # Generate colors for clusters
+    gt_colors = generate_colors(num_gt_clusters, 'Blues')       # Colormap for GT clusters
+    pred_colors = generate_colors(num_pred_clusters, 'Oranges') # Colormap for Predicted clusters
+
+    # Combine colors
+    node_colors = gt_colors + pred_colors
+
+    # Initialize lists for sources, targets, and values
+    threshold = 0   # NOTE: changing threshold will disappear small pop cell types in GT
+    source_indices = []
+    target_indices = []
+    values = []
+
+    for gt_idx, gt_cluster in enumerate(gt_clusters):
+        for pred_idx, pred_cluster in enumerate(pred_clusters):
+            if gt_cluster in transition_matrix.index and pred_cluster in transition_matrix.columns:
+                count = transition_matrix.at[gt_cluster, pred_cluster]
+                if count > threshold:
+                    source_indices.append(gt_idx)
+                    target_indices.append(pred_idx + num_gt_clusters)
+                    values.append(count)
+
+
+    # Create the Sankey diagram
+    fig = go.Figure(data=[go.Sankey(
+        node=dict(
+            pad=15,
+            thickness=20,
+            label=labels,
+            color=node_colors
+        ),
+        link=dict(
+            source=source_indices,
+            target=target_indices,
+            value=values
+        )
+    )])
+    if title is not None:
+        title_text=title
+    else:
+        title_text="Cluster Transition Sankey Diagram"
+
+    # Update layout
+    fig.update_layout(
+        title_text=title_text,
+        font_size=10,
+        width=1000,    # Adjust width as needed
+        height=800     # Adjust height as needed
+    )
+
+    # Display the diagram
+    fig.show()
+
+    if save_name is not None:
+        save=save_name
+    else:
+        save='sankey_diagram'
+    
+    if save_format=='jpg':
+        # Export as JPEG
+        fig.write_image(save+'.jpg')
+    elif save_format=='pdf':
+        # Export as PDF
+        fig.write_image(save+'.pdf')
+    elif save_format=='svg':
+        # Export as SVG
+        fig.write_image(save+'.svg')
+    else:
+        # Export as PNG (default if no extension is specified)
+        fig.write_image(save+'.png')
