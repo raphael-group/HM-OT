4f399303a1af1f7d9a13992edb0929ebfb0fc555
the-ninth-wave <34815796+the-ninth-wave@users.noreply.github.com>
2024-10-16 12:18:57 -0400

    added silhouette function to util_zf

--- util_zf.py
+++ util_zf.py
@@ -7,9 +7,13 @@ import networkx as nx
 from sklearn.neighbors import kneighbors_graph
 from scipy.spatial import distance
 from scipy.stats import entropy
+from scipy import sparse
+from scipy.sparse import linalg as splinalg
 
 from sklearn.metrics import adjusted_mutual_info_score as ami
 from sklearn.metrics import adjusted_rand_score as ari
+from sklearn.metrics import silhouette_score
+
 
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
@@ -50,118 +54,6 @@ def factor_mats(C, A, B, device, z=None, c=100, nidx_1=None, nidx_2=None):
                                        (V2_A.type(torch.DoubleTensor).to(device)/norm3, V2_B.type(torch.DoubleTensor).to(device)/norm3))
     
     return C_factors, A_factors, B_factors
-
-################################################################################################
-#   magic cost matrix
-################################################################################################
-
-def make_graph_from_coords(S, n_neighbors=4, draw_graph=False):
-    G = kneighbors_graph(X=S,
-                         n_neighbors=n_neighbors,
-                         mode='connectivity',
-                         metric='minkowski',
-                         p=2,
-                         metric_params=None,
-                         include_self=False,
-                         n_jobs=None)
-    G = nx.from_scipy_sparse_array(G)
-
-    if draw_graph:
-        nx.draw(G,
-                pos=list(S),
-                with_labels=False,
-                node_size=5)
-    else: pass
-
-    return G
-
-
-def make_Lap_ei(S, X, n_neighbors=4, draw_graph=False):
-    G = make_graph_from_coords(S=S, n_neighbors=n_neighbors,draw_graph=draw_graph)
-    Adj = nx.adjacency_matrix(G).todense()
-
-    A = distance.cdist(X,X)
-    wAdj = A * Adj
-    wAdj_row_sums = np.sum(wAdj, axis=1)
-    D = np.diag(wAdj_row_sums)
-    Lap = D = wAdj
-
-    D_inv_sqrt = np.linalg.inv(np.sqrt(D))
-    Lap_star = D_inv_sqrt @ Lap @ D_inv_sqrt
-
-    eigenvalues, eigenvectors = np.linalg.eigh(Lap_star) 
-    return eigenvectors, eigenvalues
-
-
-def HDM(spot_index, eigenvectors, eigenvalues, truncation=None, time=10.):
-    """
-    Returns embedding of spot into R^{n-1}, where
-    n is the number of eigenvalues, as well as the dimension of the eigenvectors
-
-    Args:
-        spot_index: int, index of spot in synthetic tissue
-        eigenvectors: np.ndarray, eigenvectors of normalized sheaf Laplacian
-        eigenvalues: np.ndarray, eigenvalues of normalized sheaf Laplacian
-        truncation: int or None, optional truncation of eigenvalues/vectors
-        time: float, "diffusion time"
-
-    Returns:
-        HDM_vec: np.ndarray, of length n-1 (or truncation-1)
-    """
-    n = eigenvectors.shape[1]
-    if truncation is None:
-        ell_range = np.arange(1, n)
-    else:
-        ell_range = np.arange(1, truncation)
-
-    # Compute the eigenvalues raised to the power of 'time'
-    evals = eigenvalues[ell_range] ** time  # Shape: (n-1,) or (truncation-1,)
-
-    # Get the eigenvector entries for the given spot_index
-    entries = eigenvectors[spot_index, ell_range]  # Shape: (n-1,) or (truncation-1,)
-
-    # Element-wise multiplication
-    HDM_vec = evals * entries  # Shape: (n-1,) or (truncation-1,)
-
-    return HDM_vec
-
-
-def HDM_representation(S, eigenvectors, eigenvalues, truncation=None, time=10.):
-    """
-    Returns HDM embedding stacked across all slice spots
-
-    Args:
-        S: np.ndarray, stack of n slice spatial coordinates
-        eigenvectors: np.ndarray, eigenvectors of normalized sheaf Laplacian
-        eigenvalues: np.ndarray, eigenvalues of normalized sheaf Laplacian
-        truncation: int or None, optional truncation of eigenvalues/vectors
-        time: float, "diffusion time"
-
-    Returns:
-        HDM_stack: np.ndarray, stack of n HDM representations, each
-                   of which is a row-vector of length n - 1 (or truncation - 1)
-    """
-    n = eigenvectors.shape[0]
-    if truncation is None:
-        ell_range = np.arange(1, eigenvectors.shape[1])
-    else:
-        ell_range = np.arange(1, truncation)
-
-    # Compute the eigenvalues raised to the power of 'time'
-    evals = eigenvalues[ell_range] ** time  # Shape: (n-1,) or (truncation-1,)
-
-    # Get the eigenvector entries for all spots and selected eigenvalues
-    entries = eigenvectors[:, ell_range]  # Shape: (n, n-1) or (n, truncation-1)
-
-    # Multiply each column (eigenvector) by the corresponding eigenvalue
-    HDM_stack = entries * evals  # Broadcasting over columns
-
-    return HDM_stack  # Shape: (n, n-1) or (n, truncation-1)
-
-def HDM_from_XS(S, X, n_neighbors=4, truncation=100, time=10.):
-    eigenvectors, eigenvalues = make_Lap_ei(S, X, n_neighbors)
-    HDM_stack = HDM_representation(S, eigenvectors, eigenvalues, truncation, time)
-    return HDM_stack
 
 
 ################################################################################################
@@ -286,27 +178,20 @@ def compare_T_entropies(Ts_ann, Ts_pred):
         T_ann, T_pred = T_pair
         ent_ann = entropy(T_ann.cpu().numpy().flatten())
         ent_pred = entropy(T_pred.flatten())
-        print(f'The entropy of anno transitions from {i} to {i+1} is {ent_ann:.3f}')
-        print(f'The entropy of pred transitions from {i} to {i+1} is {ent_pred:.3f}')
         if ent_pred > ent_ann:
-            print(f'Pred transitions from {i} to {i+1} are **MORE** entropic than anno transitions')
+            print(f'Pred transitions {i} -> {i+1} are **MORE** entropic: {ent_pred:.3f} > {ent_ann:.3f}')
         else:
-            print(f'Pred transitions from {i} to {i+1} are **LESS** entropic than anno transitions')
-        print('\n')
+            print(f'Pred transitions {i} to {i+1} are **LESS** entropic: {ent_pred:.3f} < {ent_ann:.3f}')
 
 def compare_T_col_entropies(Ts_ann, Ts_pred):
     for i, T_pair in enumerate(zip(Ts_ann, Ts_pred)):
         T_ann, T_pred = T_pair
         ent_ann = compute_column_entropy(T_ann.cpu().numpy())
         ent_pred = entropy(T_pred.flatten())
-        print(f'The column entropy of anno transitions from {i} to {i+1} is {ent_ann:.3f}')
-        print(f'The column entropy of pred transitions from {i} to {i+1} is {ent_pred:.3f}')
         if ent_pred > ent_ann:
-            print(f'Pred transitions from {i} to {i+1} have **MORE** column entropy than anno transitions')
+            print(f'Pred transitions {i} -> {i+1} have **MORE** column entropy: {ent_pred:.3f} > {ent_ann:.3f}')
         else:
-            print(f'Pred transitions from {i} to {i+1} have **LESS** column entropy than anno transitions')
-        print('\n')
-
+            print(f'Pred transitions {i} -> {i+1} have **LESS** column entropy: {ent_pred:.3f} < {ent_ann:.3f}')
 
 ################################################################################################
 #   computing ARIs, AMIs
@@ -343,3 +228,30 @@ def compute_ARI_and_AMI(gt_types_list, pred_types_list, x_percent=5):
         print(f'ARI for {i}th slice is {raw_ari:.3f} (filtered: {x_ari:.3f}) \t')
         print(f'AMI for {i}th slice is {raw_ami:.3f} (filtered: {x_ami:.3f})')
         print('\n')
+
+
+################################################################################################
+#   computing silhouette scores
+################################################################################################
+
+def silhouette(gt_types_list, pred_types_list, Xs, Ss):
+    for i, pair in enumerate(zip(gt_types_list, pred_types_list)):
+        gt_types, pred_types = pair
+        expr_score_gt = silhouette_score(Xs[i], gt_types)
+        expr_score_pred = silhouette_score(Xs[i], pred_types)
+        if expr_score_pred > expr_score_gt:
+            print(f'\tPred clusters {i} have **HIGHER** expression silhouette score: {expr_score_pred:.3f} > {expr_score_gt:.3f}')
+        else:
+            print(f'\tPred clusters {i} have **LOWER** expression silhouette score: {expr_score_pred:.3f} < {expr_score_gt:.3f}')
+    
+    print('\n')
+
+    for i, pair in enumerate(zip(gt_types_list, pred_types_list)):
+        gt_types, pred_types = pair
+        spa_score_gt = silhouette_score(Ss[i], gt_types)
+        spa_score_pred = silhouette_score(Xs[i], pred_types)
+        if spa_score_pred > spa_score_gt:
+            print(f'\tPred clusters {i} have **HIGHER** spatial silhouette score: {spa_score_pred:.3f} > {spa_score_gt:.3f}')
+        else:
+            print(f'\tPred clusters {i} have **LOWER** spatial silhouette score: {spa_score_pred:.3f} < {spa_score_gt:.3f}')
+        
